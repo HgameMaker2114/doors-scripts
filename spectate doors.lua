45ff0676b9b5a9bcefa9faf19aae8c2b0d652652
HgameMaker2114 <101846538+HgameMaker2114@users.noreply.github.com>
2023-05-17 18:30:29 -0500

    Add files via upload

---
+++ spectate doors.lua
@@ -0,0 +1,32 @@
+-- press r
+-- Variables
+local person = -- put the players username here
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
+
+-- Settings
+
+local SizeChange = 0
+local Camera = -30
+bind = "j"
+
+mouse.KeyDown:connect(function(key)
+    if key == bind then
+        SizeChange = SizeChange - 100
+    end
+end)
+for i,v in pairs(game.Players.person.Character:GetChildren()) do
+    if v:IsA("MeshPart") then
+        v.Transparency = 1
+    end
+    if v:IsA("Part") then
+        v.Transparency = 1
+    end
+    if v:IsA("Accessory") then
+        v:Destroy()
+    end
+    game.Players.person.Character.Head.face.Transparency = 1
+end
+local Con = game:GetService("RunService").RenderStepped:Connect(function()
+    game.Workspace.CurrentCamera.CFrame = (game.Players.person.Character.Head.CFrame * CFrame.Angles(math.rad(Camera),0,0)) * CFrame.new(0,SizeChange,0)
+end)
